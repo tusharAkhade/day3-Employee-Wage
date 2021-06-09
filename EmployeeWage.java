dc1c2807fa71353984e89831ca796798e0f3e20e
Tushar Akhade <tusharakhade10@gmail.com>
2021-06-09 16:34:00 +0530

    added condition to calculate wage till total working hours or days reached

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,11 +5,13 @@ public class EmployeeWage
 		int is_fulltime_present = 1;
 		int is_parttime_present = 2;
 		int wage_per_hr = 20;
-		int working_hr=0;
+		int working_hr = 0;
+		int total_working_hr = 0;
 		int days_in_month=20;
+		int max_working_hrs = 100;
 		int monthly_total_wage = 0;
 		int daily_wage_array[] = new int[20];
-		for (int i=0; i<days_in_month; i++) {
+		for (int i=0; i<days_in_month && total_working_hr<max_working_hrs; i++) {
 			int emp_check = (int)(Math.floor(Math.random() * 10)) % 3;
 			switch (emp_check) {
 				case 1:
@@ -21,9 +23,9 @@ public class EmployeeWage
 				default:
 					working_hr = 0;
 			}
+			total_working_hr += working_hr;
 			daily_wage_array[i] = working_hr * wage_per_hr;
 		}
-
 		for (int j=0; j<days_in_month; j++) {
 			int day = j+1;
 			System.out.println("Day " + day + " wage is " + daily_wage_array[j]);
